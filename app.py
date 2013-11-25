7ef706a02ea282f3be49629bbdc916f8ba760bb4
Raymon Sutedjo-The <raymon.sutedjothe@gmail.com>
2013-11-25 13:19:47 -0800

    removed pipes

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ redis = redis.from_url(REDIS_URL)
 @app.route('/')
 def index():
     visitors = redis.get('visitors')
-    os = redis.get('osdata') ||
+    os = redis.get('osdata')
 
     num = 0 if visitors is None else int(visitors)
     num += 1
