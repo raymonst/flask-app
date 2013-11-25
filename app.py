995692002d811fb8012e9e454a6ad6a299b343e7
Raymon Sutedjo-The <raymon.sutedjothe@gmail.com>
2013-11-25 13:05:50 -0800

    removed redis

--- app.py
+++ app.py
@@ -3,19 +3,21 @@ from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
-REDIS_URL = os.getenv('REDISTOGO_URL', 'redis://localhost:5000')
-redis = redis.from_url(REDIS_URL)
+#REDIS_URL = os.getenv('REDISTOGO_URL', 'redis://localhost:5000')
+#redis = redis.from_url(REDIS_URL)
 
 
 @app.route('/')
 def index():
-    visitors = redis.get('visitors')
-    os = redis.get('osdata') ||
+    #visitors = redis.get('visitors')
+    #os = redis.get('osdata') ||
 
-    num = 0 if visitors is None else int(visitors)
-    num += 1
+    #num = 0 if visitors is None else int(visitors)
+    #num += 1
 
-    redis.set('visitors', num)
+    num = 30
+
+    #redis.set('visitors', num)
     return render_template('index.html', number=num)
 
 if __name__ == '__main__':
